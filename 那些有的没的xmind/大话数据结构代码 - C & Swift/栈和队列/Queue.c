3749441d70ec6ec20dc035eebef873dfbcfb4d7c
Azen <azen.cell@foxmail.com>
2017-02-11 17:09:02 +0800

    串 	- next 模式匹配

---
+++ 那些有的没的xmind/大话数据结构代码 - C & Swift/栈和队列/Queue.c
@@ -0,0 +1,16 @@
+/*
+ ADT 队列(Queue)
+ Data
+	同线性表。元素具有相同的数据类型，相邻元素具有直接的前驱后继关系
+ Operation
+	InitQueue(*Q) 初始化，建立一个空队列
+	DestroyQueue(*Q)
+	ClearQueue(*Q)
+	QueueEmpty(Q)
+	GetHead(Q, *e)
+	EnQueue(*Q, e)
+	DeQueue(*Q, *e)
+	QueueLength(Q)
+ endADT
+ 
+ */
