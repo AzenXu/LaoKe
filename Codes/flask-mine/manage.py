9ba9e1ec9496ff7049c878a4a2308558620e8da9
XuAzen <xuchang08@meituan.com>
2017-05-06 20:41:55 +0800

    启动脚本加注释 & 创建requirements.txt文件 - 类似podfile

--- Codes/flask-mine/manage.py
+++ Codes/flask-mine/manage.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
-
-# 第1行的作用 - 直接使用./manager.py执行脚本，而不需要使用python manager.py - shebang声明
+# 上面一行的作用 - 直接使用./manager.py执行脚本，而不需要使用python manager.py - shebang声明
 
 import os
-from app import create_app, db
-from app.models import User, Role
-from flask_script import Manager, Shell
-from flask_migrate import Migrate, MigrateCommand
+from app import create_app, db # 从app这个文件夹下，找到creat_app和db包含进来
+from app.models import User, Role # 从app下的models模块 - 文件， 找到User和Role引进来
+from flask_script import Manager, Shell # 引进来命令行管家
+from flask_migrate import Migrate, MigrateCommand # 数据库迁移
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default') # 从环境变量里读取配置名或使用default
 
@@ -14,6 +13,7 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default') # 从环境变量里读
 manager = Manager(app)
 migrate = Migrate(app, db)
 
+# 为Shell配置数据库，和数据库数据库迁移 - 可以直接通过Shell便捷的操纵数据库
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
 manager.add_command("shell", Shell(make_context=make_context=make_shell_context))
