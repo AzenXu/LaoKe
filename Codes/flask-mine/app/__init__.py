0a58dc8a55dec9c37c37a7aea425c232251ef2d0
XuAzen <xuchang08@meituan.com>
2017-05-10 09:47:30 +0800

    用户体系开始搭建…

--- Codes/flask-mine/app/__init__.py
+++ Codes/flask-mine/app/__init__.py
@@ -3,6 +3,10 @@
 # 这是一个构造函数，app对象从这里生成，以方便进行配置
 # 在这里导入需要用到的库，但是不初始化（仅实例化），因为app还没生成
 
+# 这里引入记录登录状态的库 - 书8.4节
+from flask_login import LoginManager
+
+# --- 下面是旧的 ---
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
@@ -12,6 +16,13 @@ from config import config # 导入自定义的config字典
 bootstrap = Bootstrap()
 mail = Mail()
 db = SQLAlchemy()
+
+#登录状态记录
+login_manager = LoginManager()
+# 设为strong会记录客户端IP和浏览器的用户代理信息，发现异常就登出
+login_manager.session_protection = 'strong'
+# 设置登录页面的端点 - auth.这个是定义了路由的蓝本的名字
+login_manager.login_view = 'auth.login'
 
 # 实现工厂函数，创建app实例
 def create_app(config_name):
@@ -23,8 +34,17 @@ def create_app(config_name):
     mail.init_app(app)
     db.init_app(app)
 
+    login_manager.init_app(app)
+
     # 使用蓝本定义路由 - 蓝本描述了路由和错误处理
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    # 注册用户蓝本
+    from .auth import auth as auth_blueprint
+    # url_prefix参数 - 以后在蓝本中定义的所有路由，都会加上指定的前缀
+    # 如: /login路由会注册成/auth/login
+    # 对应的url: http://localhost:5000/auth/login
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
--- Codes/flask-mine/app/models.py
+++ Codes/flask-mine/app/models.py
@@ -2,6 +2,18 @@
 
 from . import db
 
+# 这里引入加密库
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# 这里引入了记录用户登录状态的库
+from flask_login import UserMixin
+
+# 加载用户的回调函数 - login_manager需要实现的方法
+from . import login_manager
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id)) # 返回用户对象，没有返回None
+
 # 定义两个表 - 通过定义类的方式
 class Role(db.Model):
     __tablename__ = 'roles' # 这个变量定义了在数据库中使用的表名
@@ -12,11 +24,25 @@ class Role(db.Model):
     def __repr__(self): # 可选方法，同iOS类的description方法
         return '<Role %r>' % self.name
 
-class User(db.Model):
+class User(UserMixin, db.Model): # 传说中的多继承？
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), unique = True, index = True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id')) # 设置外键，关联roles表中的id值
+    email = db.Column(db.String(64), unique = True, index = True)
+
+    # 学习用户认证的童鞋看这里
+    password_hash = db.Column(db.String(128))
+    @property # - 这里学习get、set方法的定义
+    def password(self, password):
+        raise AttributeError('password is not a readable attribute')
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    # 这里定义一个成员方法
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User %>' % self.username
