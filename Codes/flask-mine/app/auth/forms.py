512ddc96df0408881181c66a9794cad2d0e989b0
XuAzen <xuchang08@meituan.com>
2017-05-12 10:07:15 +0800

    注册功能 done~~

--- Codes/flask-mine/app/auth/forms.py
+++ Codes/flask-mine/app/auth/forms.py
@@ -2,10 +2,30 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, length, Email
+from wtforms.validators import Required, length, Email, regexp, EqualTo
+from wtforms import ValidationError
+from ..models import User
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[Required(), length(1, 64), Email()])
     password = PasswordField('Password', validators=[Required()])
     remember_me = BooleanField('Keep me logged in') # 复选框
     submit = SubmitField('Log In')
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[Required(), length(1, 64), Email()])
+    username = StringField('Username', validators=[Required(), length(1, 64), regexp('^[A-Za-z][A-Za-z0-9_.]*$',0,'Usernames must have only letters,numbers,dots or underscores')])
+    password = PasswordField('Password', validators=[Required(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm password', validators=[Required()])
+    submit = SubmitField('Register')
+
+    # 如果定义了以validate_开头且后面跟着「字段名」的方法，这个方法就和验证函数一起调用
+    # 定义了email的验证函数
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            # 通过抛ValidationError的方式来验证失败
+            raise ValidationError('Email alread registered.')
+    # 定义了username的验证函数
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('username already in use.')
--- Codes/flask-mine/app/auth/views.py
+++ Codes/flask-mine/app/auth/views.py
@@ -5,7 +5,8 @@ from flask_login import login_user
 from flask_login import logout_user, login_required
 from . import auth
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
+from .. import db
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -27,3 +28,17 @@ def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(
+            email=form.email.data,
+            username=form.username.data,
+            password=form.password.data
+        )
+        db.session.add(user)
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
