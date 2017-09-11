b1968e29d31586a05322b66731f4b854df368389
Azen Xu <xuchang08@meituan.com>
2017-09-11 16:38:42 +0800

    模板访问用户权限检查功能

--- Codes/flask-mine/app/main/__init__.py
+++ Codes/flask-mine/app/main/__init__.py
@@ -5,8 +5,16 @@
 
 from flask import Blueprint
 
-main = Blueprint('main', __name__) # 创建蓝本 - 蓝本名字， 蓝本所在的包
+from app.models import Permission
+
+main = Blueprint('main', __name__)  # 创建蓝本 - 蓝本名字， 蓝本所在的包
 
 from . import views, errors
 
+
 # 导入进来，就意味着这两个文件中的代码会被执行！这和编译型语言非常不同...
+
+#  使用上下文处理器，让模板可能检查权限
+@main.app_context_processor
+def inject_permissions():
+    return dict(Permission=Permission)
